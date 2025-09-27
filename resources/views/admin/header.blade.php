5e61b6087d4336ba8adaf0319252d96a6d5cf138
Cássia Polleto <cassiapolleto@gmail.com>
2025-09-27 07:43:03 -0300

    feat: add patients_invoicing

--- resources/views/admin/header.blade.php
+++ resources/views/admin/header.blade.php
@@ -4,21 +4,11 @@
         <!-- Sidebar -->
         <nav class="col-md-2 d-none d-md-block bg-white sidebar py-4">
             <div class="text-center mb-4">
-                <i class="bi bi-hospital fs-1 text-primary"></i>
-                <h5 class="mt-2">Hospital Vida</h5>
+                <img src="{{ asset('assets/img/logo.png') }}" alt="" width="200">
             </div>
             <ul class="nav flex-column">
                 <li class="nav-item">
-                    <a class="nav-link active" href="#"><i class="bi bi-speedometer2 me-2"></i>Dashboard</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#"><i class="bi bi-people me-2"></i>Pacientes</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#"><i class="bi bi-file-earmark-text me-2"></i>Faturas</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#"><i class="bi bi-calendar-check me-2"></i>Consultas</a>
+                    <a class="nav-link active" href="#"><i class="bi bi-file-earmark-text me-2"></i>Faturamento</a>
                 </li>
             </ul>
         </nav>
